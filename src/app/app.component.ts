f03850524cd8426de50e4ce2ad65a275728eab6e
Max Carlquist <tenkaklet@gmail.com>
2019-04-04 15:30:20 +0200

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireList } from '@angular/fire/database';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Database';
+  title: '';
+  todoItem: '';
+  todos: any[];
+  constructor(private database: AngularFireDatabase) {}
+
+  ngOnInit() {
+    const ref = this.database.list('todos');
+    ref.valueChanges().subscribe(value => {
+      this.todos = value;
+      console.log(this.todos);
+    });
+  }
+
+  addtodo(form: NgForm) {
+    console.log(form.value);
+    this.database.list('/todos').push(form.value);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
